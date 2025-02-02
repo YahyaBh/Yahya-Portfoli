a54f7f0887ac3bca26858fbf192f5082c0012ffd
Yahya Bouhsine <81886762+YahyaBh@users.noreply.github.com>
2025-02-02 21:43:36 +0100

    Config

--- frontend_client/src/client.js
+++ frontend_client/src/client.js
@@ -4,9 +4,9 @@ import imageUrlBuilder from '@sanity/image-url';
 export const client = sanityClient({
     projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
-    apiVersion: '2022-09-16',
+    apiVersion: '2025-01-15',
     useCdn: true,
-    token: process.env.REACT_APP_SANITY_TOKEN,
+    token: process.env.REACT_APP_SANITY_TOKEN  || 'skKNP2Y7VCiVvj7BKpYgg7XslkMdS137Fhbl9RrQWVpnwLFu4eHDRd2165C8viu6qnGdTRT2624L850ht9web85uwfa06o0qrcfcdYdct372Wv1yQc1XZ4VMbPNIRRrmtwW1W54hj06lwDXxCcnzxqdo4KVXtGtopnEPOhl9bsycJW1BcRt3',
     ignoreBrowserTokenWarning: true
 });
 
